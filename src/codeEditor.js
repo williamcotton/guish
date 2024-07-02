1a17545ed96990bdd0725e616d4e3680dcfcbe7f
William Cotton <williamcotton@gmail.com>
2024-07-02 16:09:56 -0500

    update code editor styling

--- src/codeEditor.js
+++ src/codeEditor.js
@@ -5,16 +5,23 @@ const CodeEditorComponent = ({ value, onChange, language }) => {
   return (
     <Editor
       height="100%"
-      width="100%"
+      className="border rounded p-2"
       loading={false}
       language={language}
       value={value}
       onChange={onChange}
       options={{
-        minimap: { enabled: false },
+        minimap: { enabled: false, showSlider: "mouseover" },
         scrollBeyondLastLine: false,
         fontSize: 14,
         automaticLayout: true,
+        lineNumbers: "off",
+        glyphMargin: false,
+        folding: false,
+        renderLineHighlight: "none",
+        lineDecorationsWidth: 0,
+        lineNumbersMinChars: 0,
+        overviewRulerLanes: 0,
       }}
     />
   );
