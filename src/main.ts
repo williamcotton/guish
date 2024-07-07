fa448c04fef8d7af08f8bcf5eb2816b27ff9166e
William Cotton <williamcotton@gmail.com>
2024-07-07 05:51:33 -0500

    use electron-forge for build

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,6 @@ import { spawn } from "child_process";
 import fs from "fs";
 import os from "os";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 interface Config {
   shell: string;
   preloadScript: string;
@@ -41,14 +37,18 @@ try {
   console.warn("Using default configuration");
 }
 
-function createWindow() {
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1500,
     height: 1000,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: path.resolve(app.getAppPath(), 'dist', 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
@@ -66,15 +66,17 @@ function createWindow() {
     }
   }
 
-  mainWindow.loadURL(
-    isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    );
   }
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 
   // Create the Application's main menu
   const template: (Electron.MenuItemConstructorOptions | Electron.MenuItem)[] =
@@ -286,10 +288,16 @@ function createWindow() {
       }
     }
   );
-}
+};
 
-app.whenReady().then(createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -297,7 +305,12 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
