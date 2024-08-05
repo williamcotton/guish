4893a4bb191f7092bf6356d9d13360d797881d10
William Cotton <williamcotton@gmail.com>
2024-08-04 19:58:07 -0500

    use more concise jq

--- demo/weather-facets.sh
+++ demo/weather-facets.sh
@@ -1,9 +1,7 @@
-curl -s 'https://api.open-meteo.com/v1/forecast?latitude=30.27&longitude=-97.74&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m' |
-  jq -r '.hourly |
-    ["time", "temperature_2m", "relative_humidity_2m", "wind_speed_10m"],
-    ([.time, .temperature_2m, .relative_humidity_2m, .wind_speed_10m] | transpose[]) |
-    @csv' |
-  ggplot 'df_long <- pivot_longer(df, cols = c("temperature_2m", "relative_humidity_2m", "wind_speed_10m"),
+curl -s 'https://api.open-meteo.com/v1/forecast?latitude=30.27&longitude=-97.74&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m' | jq -r '.hourly |
+    keys_unsorted,
+    transpose[] |
+    @csv' | ggplot 'df_long <- pivot_longer(df, cols = c("temperature_2m", "relative_humidity_2m", "wind_speed_10m"),
                           names_to = "variable", values_to = "value")
 
 # Rename the variables for better legibility
@@ -21,5 +19,4 @@ df_long$variable[df_long$variable == "wind_speed_10m"] <- "Wind (m/s)"
     facet_wrap(~ variable, scales = "free_y", nrow = 3) +
     labs(title = "Weather Data in Austin, TX", x = "Time", y = "Value") +
     scale_x_datetime(date_breaks = "1 day", date_labels = "%b %d") +
-    theme_minimal()' |
-  pngtohtml
+    theme_minimal()' | pngtohtml
--- demo/weather.sh
+++ demo/weather.sh
@@ -1,6 +1,6 @@
 curl -s 'https://api.open-meteo.com/v1/forecast?latitude=30.27&longitude=-97.74&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m' | jq -r '.hourly | 
-  ["time", "temperature_2m", "relative_humidity_2m", "wind_speed_10m"],
-  ([.time, .temperature_2m, .relative_humidity_2m, .wind_speed_10m] | transpose[]) | 
+  keys_unsorted, 
+  transpose[] | 
   @csv' | ggplot 'df_long <- pivot_longer(df, cols = c("temperature_2m", "relative_humidity_2m", "wind_speed_10m"),
                         names_to = "variable", values_to = "value")
 
